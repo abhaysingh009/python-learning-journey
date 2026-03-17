fb1075c819513f345c8f9b66a7ee24183694dd68
ABHAY PRATAP SINGH <abhayv178@gmail.com>
2026-03-17 22:22:30 +0530

    Add files via upload

---
+++ Python/02.control_flow/conditional.py
@@ -0,0 +1,16 @@
+a=int(input("Enter Number: "))
+
+
+# if(a%2==0):
+#     print("Even");
+# else:
+#     print("odd");
+
+if(a>0):
+    print(a,"Positive");
+    print("Number");
+elif(a<0):
+    print(a,"Negative")
+    print("Number");
+else:
+    print(a,"is 0");
